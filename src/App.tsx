998fefbe074071b9f7b98456ccfbdf8631565245
fenricage <fenricage@gmail.com>
2022-08-27 22:32:32 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GLView from "./components/GLView";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <GLView/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/GLView.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react'
+import styled from 'styled-components/macro'
+
+const GLViewContainer = styled.div`
+
+`
+
+
+const GLView: FC = () => {
+
+  return (
+    <GLViewContainer>
+      1
+    </GLViewContainer>
+  );
+};
+
+export default GLView;
